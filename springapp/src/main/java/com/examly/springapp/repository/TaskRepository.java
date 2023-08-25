aa7aaa7e0fad51dcf1eb6c1705451a7d082ccf73
Iamneo <techadmin@iamneo.ai>
2023-08-25 15:40:43 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/repository/TaskRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/TaskRepository.java
@@ -1,4 +1,4 @@
-import com.examly.springapp.repository;
+import com.examly.springapp.entity.Taskentity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
