44908a55efa3d03e35045bc967d60865228dd831
Iamneo <techadmin@iamneo.ai>
2023-08-25 14:13:08 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/repository/TasksRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/TasksRepository.java
@@ -1,8 +1,12 @@
 package com.examly.springapp.repository;
 
-import org.springframework.data.repository.CrudRepository;
-import com.examly.springapp.model.Task;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TasksRepository extends CrudRepository<Task, String> {
+    // Other methods...
     
+    @Modifying
+    @Query("UPDATE Task t SET t.taskStatus = :taskStatus WHERE t.taskId = :taskId")
+    void updateTaskStatus(String taskId, String taskStatus);
 }
