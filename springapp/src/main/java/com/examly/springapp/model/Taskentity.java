633338a1a10bed90e0957d1e507c63bfe4ed9d7c
Iamneo <techadmin@iamneo.ai>
2023-08-25 14:48:20 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/model/Taskentity.java
+++ springapp/src/main/java/com/examly/springapp/model/Taskentity.java
@@ -1,53 +1,58 @@
-package com.examly.springapp.model;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Column;
 
 @Entity
-@Table
-public class Task {
-	@Id
-    @Column
-	private String taskId;
-    @Column
-	private String taskHolderName;
-    @Column
-	private String taskDate;
-    @Column
-	private String taskName;
-    @Column
-	private String taskStatus;
+@Table(name="Task")
+public class Taskentity {
 
-	public String getTaskId() {
-		return taskId;
-	}
-	public void setTaskId(String taskId) {
-		this.taskId = taskId;
-	}
-    public String getTaskHolderName() {
-		return taskHolderName;
-	}
-	public void setTaskHolderName(String taskHolderName) {
-		this.taskHolderName = taskHolderName;
-	}
-    public String getTaskDate() {
-		return taskDate;
-	}
-	public void setTaskDate(String taskDate) {
-		this.taskDate = taskDate;
-	}
-	public String getTaskName() {
-		return taskName;
-	}
-	public void setTaskName(String taskName) {
-		this.taskName = taskName;
-	}
-    public String getTaskStatus() {
-		return taskStatus;
+    @Id
+    private String taskId;
+    private String taskHolderName;
+    private String taskDate;
+    private String taskName;
+    private String taskStatus;
+   
+
+    public String getTaskId() {
+        return taskId;
     }
-	public void setTaskStatus(String taskStatus) {
-		this.taskStatus = taskStatus;
-	}
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+    public String getTaskHolderName() {
+        return taskHolderName;
+    }
+    public void setTaskHolderName(String taskHolderName) {
+        this.taskHolderName = taskHolderName;
+    }
+    public String getTaskDate() {
+        return taskDate;
+    }
+    public void setTaskDate(String taskDate) {
+        this.taskDate = taskDate;
+    }
+    public String getTaskName() {
+        return taskName;
+    }
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+    public String getTaskStatus(){
+        return taskStatus;
+    }
+    public void setTaskStatus(String taskStatus){
+        this.taskStatus = taskStatus;
+    }
+
+    public Taskentity(String taskId, String taskHolderName, String taskDate, String taskName, String taskStatus) {
+        this.taskId = taskId;
+        this.taskHolderName = taskHolderName;
+        this.taskDate = taskDate;
+        this.taskName = taskName;
+        this.taskStatus = taskStatus;
+    }
+  public Taskentity(){
+      
+  }  
 }
