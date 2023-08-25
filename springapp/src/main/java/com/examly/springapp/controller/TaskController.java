fa4ca8335ad00255988dbed4914d90a0457abce9
Iamneo <techadmin@iamneo.ai>
2023-08-25 13:21:19 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/TaskController.java
+++ springapp/src/main/java/com/examly/springapp/controller/TaskController.java
@@ -1,59 +1,47 @@
-package com.examly.springapp.taskmanagement;
+package com.examly.springapp.controller;
 
-
-// controlls the logic from DB 
+import com.examly.springapp.entity.Taskentity;
+import com.examly.springapp.service.TaskService;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping()
 public class TaskController {
-    
     @Autowired
-    private TaskService taskService;
-
-    @GetMapping("/")
-    public String sayHello(){
-        return "Hello From /";
+    private TaskService service;
+    
+    @PostMapping("/saveTask")
+    public Taskentity saveTask(@RequestBody Taskentity taskentity){
+        Taskentity output = service.saveTask(taskentity);
+        return output;
     }
 
+
+    @GetMapping("/changeStatus")
+    private Taskentity updatetaskStatus(@RequestParam("id") String id){
+        return service.updatetaskStatus(id);
+    }
+
+    @GetMapping("/deleteTask")
+    private String deleteTask(@RequestParam("id") String id){
+        return service.deleteTask(id);
+    }
     @GetMapping("/alltasks")
-    public List<Task> getAllTasks() {
-        return taskService.getAllList();
+    private List<Taskentity> getallTasks(){
+        return service.getallTasks();
     }
 
-    @RequestMapping("/getTask")
-    public Task getTask(@RequestParam("id") Integer id) {
-        return taskService.getTask(id);
+    @GetMapping("/getTask")
+    private Taskentity getTask(@RequestParam("id") String id){
+        return service.gettaskbyid(id);
     }
-
-    @PostMapping(value="/saveTask")
-    public void createTask(@RequestBody Task task) {
-        taskService.createTask(task);
-    }
-    
-    // PUT REQ
-    @RequestMapping(value = "/changeStatus", method = RequestMethod.PUT)
-    public void updateStatus(@RequestParam("id") Integer id,  @RequestBody Task task ) { 
-        taskService.updateStatus(id, task);
-    }
-    
-    
-    // Delete Req
-    @DeleteMapping(value = "/deleteTask")
-    public void deleteTask(@RequestParam("id") Integer id) { 
-        taskService.deleteTask(id);
-    }
-    
-
 
 }
